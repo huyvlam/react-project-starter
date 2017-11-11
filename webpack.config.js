e9ce32fd5dd47b81e6be2ba55d76ad9d6f7abbdd
Huy Lam <huyster@Huys-MacBook-Pro.local>
2017-11-11 14:14:23 -0800

    update eslint, uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,46 @@
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
 
-const APP_DIR = path.resolve(__dirname, 'src')
-const BUILD_DIR = path.resolve(__dirname, 'lib')
+const APP_DIR = path.resolve(__dirname, "src")
+const BUILD_DIR = path.resolve(__dirname, "lib")
 
 module.exports = {
-  entry: APP_DIR + '/index.js',
+  entry: APP_DIR + "/index.js",
   output: {
     path: BUILD_DIR,
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: 'Tile'
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "Tile"
   },
   /* To exclude these modules in output, uncomment "externals: {}" */
   // externals: {
-  //   react: 'react',
-  //   'react-dom': 'react-dom',
-  //   'prop-types': 'prop-types'
+  //   react: "react",
+  //   "react-dom": "react-dom",
+  //   "prop-types": "prop-types"
   // },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         include: APP_DIR,
         options: {
           presets: [
-            'es2015', 'react'
+            "env", "react"
           ]
         }
       },
       {
         test: /\.jsx$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         include: APP_DIR,
         options: {
           presets: [
-            'es2015', 'react'
+            "env", "react"
           ]
         }
       }
@@ -47,20 +48,24 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true
-      },
-      compress: {
-        warnings: false,
-        screw_ie8: true
-      }
-    }),
     new HtmlWebpackPlugin({
-      template: APP_DIR  + '/index.html',
-      filename: 'index.html',
-      inject: 'body'
+      template: APP_DIR + "/index.html",
+      filename: "index.html",
+      inject: "body"
+    }),
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        ie8: false,
+        ecma: 5,
+        warnings: false,
+        compress: {
+          ecma: 6
+        },
+        output: {
+          beautify: false,
+          comments: false
+        }
+      }
     })
   ]
 }
